65ec8f79eb692990b6fbe80971e6da3d53fd015f
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-06 11:53:08 -0600

    adding more functions

--- JSfunc.js
+++ JSfunc.js
@@ -11,8 +11,32 @@ function square(num){
      return num * num;
 }
 
+function negate(num){
+     return -(num);
+}
+
+function toArray(num1, num2, num3){
+	 var arr = [num1, num2, num3];
+     return arr;
+}
+
+function startswithA(str){
+	   if (str.charAt(0) === 'A' || str.charAt(0) === 'a')
+           return true;
+       else
+       		return false;
+
+}
+
 tripleFive();
 
 console.log(lastLetter('Hello'));
 
 console.log(square(4));
+
+console.log(negate(-3));
+
+console.log(toArray(1, 2, 4));
+
+console.log(startswithA("apple"));
+
