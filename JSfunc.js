2e5492e50d92ca07f54cbd39e265b4f9994c3a84
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-06 11:42:38 -0600

    function practice

---
+++ JSfunc.js
@@ -0,0 +1,18 @@
+function tripleFive() {
+	for (var i = 0; i < 3; i++)
+	      console.log('Five!');
+}
+
+function lastLetter(str){
+     return str.charAt(str.length - 1);
+}
+
+function square(num){
+     return num * num;
+}
+
+tripleFive();
+
+console.log(lastLetter('Hello'));
+
+console.log(square(4));
